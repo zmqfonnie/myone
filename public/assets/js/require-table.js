518905333b52f61fb7c46ca3659742822a64f72b
zmqfonnie <2831412977@qq.com>
2020-03-26 16:56:20 +0800

    '更新img插件的使用'

--- public/assets/js/require-table.js
+++ public/assets/js/require-table.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'moment','viewer', 'moment/locale/zh-cn', 'bootstrap-table', 'bootstrap-table-lang', 'bootstrap-table-export', 'bootstrap-table-commonsearch', 'bootstrap-table-template'], function ($, undefined, Moment) {
+define(['jquery', 'bootstrap', 'moment', 'viewer', 'moment/locale/zh-cn', 'bootstrap-table', 'bootstrap-table-lang', 'bootstrap-table-export', 'bootstrap-table-commonsearch', 'bootstrap-table-template'], function ($, undefined, Moment) {
     var Table = {
         list: {},
         // Bootstrap-table 基础配置
@@ -371,34 +371,34 @@ define(['jquery', 'bootstrap', 'moment','viewer', 'moment/locale/zh-cn', 'bootst
                 img: {
                     'click .viewer-img': function (e, value, row, index) {
 
-                        //查找字段名
-                        function getKey(obj, value) {
-                            for (let key in obj) {
-                                if (obj[key] === value) {
-                                    //找到了 value
-                                    return key;
-                                } else {
-                                    //不是要找的 value
-                                    if (typeof obj[key] === 'object') {
-                                        //该值为对象
-                                        let temp = getKey(obj[key], value);
-
-                                        if (temp) {
-                                            //temp 不是 undefined，找到了 value
-                                            return '${temp}, ${key}';
-                                        }
-                                    }
-                                }
-                            }
-                            ;
-                        }
-
-                        var field = getKey(row, value);
+                        // //查找字段名
+                        // function getKey(obj, value) {
+                        //     for (let key in obj) {
+                        //         if (obj[key] === value) {
+                        //             //找到了 value
+                        //             return key;
+                        //         } else {
+                        //             //不是要找的 value
+                        //             if (typeof obj[key] === 'object') {
+                        //                 //该值为对象
+                        //                 let temp = getKey(obj[key], value);
+                        //
+                        //                 if (temp) {
+                        //                     //temp 不是 undefined，找到了 value
+                        //                     return '${temp}, ${key}';
+                        //                 }
+                        //             }
+                        //         }
+                        //     }
+                        //     ;
+                        // }
+                        //
+                        // var field = getKey(row, value);
 
                         var options = {
                             url: 'data-original',
                         };
-                        $('#viewer' + field + index).viewer(options);
+                        $('#viewer' + $(this).data('field') + index).viewer(options);
                     },
                 },
                 operate: {
@@ -458,39 +458,15 @@ define(['jquery', 'bootstrap', 'moment','viewer', 'moment/locale/zh-cn', 'bootst
             formatter: {
                 //图片查看器
                 img: function (value, row, index) {
-
-                    //查找字段名 使多字段使用
-                    function getKey(obj, value) {
-                        for (let key in obj) {
-                            if (obj[key] === value) {
-                                //找到了 value
-                                return key;
-                            } else {
-                                //不是要找的 value
-                                if (typeof obj[key] === 'object') {
-                                    //该值为对象
-                                    let temp = getKey(obj[key], value);
-
-                                    if (temp) {
-                                        //temp 不是 undefined，找到了 value
-                                        return '${temp}, ${key}';
-                                    }
-                                }
-                            }
-                        }
-                        ;
-                    }
-
-                    var field = getKey(row, value);
-
+                    var that = this;
                     value = value === null ? '' : value.toString();
                     var classname = typeof this.classname !== 'undefined' ? this.classname : 'img-sm viewer-img';
                     var arr = value.split(',');
-                    var ul = $('<ul id="viewer' + field + index + '" style="list-style-type:none;padding:0;margin-bottom:0px;width:100%;"></ul>');
+                    var ul = $('<ul id="viewer' + that.field + index + '" style="list-style-type:none;padding:0;margin-bottom:0px;width:100%;"></ul>');
                     $.each(arr, function (i, value) {
                         value = value ? value : '/assets/img/blank.gif';
                         if (i == 0) {
-                            ul.append('<li><img class="' + classname + '" style="cursor:pointer;margin: 0 auto;float:none;" data-original="' + Fast.api.cdnurl(value) + '" src="' + Fast.api.cdnurl(value) + '" /></li>');
+                            ul.append('<li><img class="' + classname + '" style="cursor:pointer;margin: 0 auto;float:none;" data-field=' + that.field + ' data-original="' + Fast.api.cdnurl(value) + '" src="' + Fast.api.cdnurl(value) + '" /></li>');
                         } else {
                             ul.append('<li><img class="' + classname + '" style="cursor:pointer;display:none;margin: 0 auto;float:none;" data-original="' + Fast.api.cdnurl(value) + '" src="' + Fast.api.cdnurl(value) + '" /></li>');
                         }
