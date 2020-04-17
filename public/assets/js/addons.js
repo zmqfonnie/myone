b937081c9f08c7a045e6d6ed3c99ff9141637da2
zmqfonnie <2831412977@qq.com>
2020-04-17 16:22:58 +0800

    update_test

--- public/assets/js/addons.js
+++ public/assets/js/addons.js
@@ -65,6 +65,19 @@ require.config({
 });
 require.config({
     paths: {
+        'async': '../addons/example/js/async',
+        'BMap': ['//api.map.baidu.com/api?v=2.0&ak=mXijumfojHnAaN2VxpBGoqHM'],
+    },
+    shim: {
+        'BMap': {
+            deps: ['jquery'],
+            exports: 'BMap'
+        }
+    }
+});
+
+require.config({
+    paths: {
         'webix': '../libs/webix/codebase/webix',
         'filemanager': '../libs/webix/codebase/filemanager',
     },
